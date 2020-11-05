8d896855a5f116ac5f6bb60fc6c724056f9c2bab
Axel Wulff Sæther <axelwulff@mac.com>
2020-11-05 19:35:03 +0100

    Etter 196

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,23 @@
+import {call, put, takeLatest} from 'redux-saga/effects';
+
+import ShopActionTypes from "./shop.types";
+import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
+import {fetchCollectionsFailure, fetchCollectionsSuccess} from "./shop.actions";
+
+export function* fetchCollections() {
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(
+            convertCollectionsSnapshotToMap,
+            snapshot
+        );
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+}
+
+export function* onFetchCollectionsStart() {
+    yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+}
