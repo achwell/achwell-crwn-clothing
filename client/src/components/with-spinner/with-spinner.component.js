3a3156b610a32070e10ec2711ef773561efc1d50
Axel Wulff Sæther <axelwulff@mac.com>
2020-11-16 11:23:48 +0100

    262. Error Boundaries

--- client/src/components/with-spinner/with-spinner.component.js
+++ client/src/components/with-spinner/with-spinner.component.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import Spinner from "../spinner/spinner.component";
 
-import {SpinnerContainer, SpinnerOverlay} from './with-spinner.styles';
-
-const WithSpinner = WrappedComponent => ({isLoading, ...otherProps}) => isLoading ? <SpinnerOverlay><SpinnerContainer/></SpinnerOverlay> : <WrappedComponent {...otherProps} />
+const WithSpinner = WrappedComponent => ({isLoading, ...otherProps}) => isLoading ? <Spinner/> : <WrappedComponent {...otherProps} />
 
 export default WithSpinner;
