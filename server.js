aaa62a49c9d391a81d80d7cec25d789bbc209340
Axel Wulff Sæther <axelwulff@mac.com>
2020-11-17 14:17:19 +0100

    PWA

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ app.listen(port, error => {
 
 app.get('/reportWebVitals.js', (req, res) => {
     res.sendFile(path.resolve(__dirname), '..', 'build', 'reportWebVitals.js');
-})
+});
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname), '..', 'build', 'service-worker.js');
+});
 
 app.post('/payment', (req, res) => {
     const body = {
